526408f01113d9ee9ced42cd985bb39888783c48
snehas-SJSU <sneha.singh@sjsu.edu>
2025-11-04 12:11:58 -0800

    Update user_session_channel.sql

--- build_mau/models/input/user_session_channel.sql
+++ build_mau/models/input/user_session_channel.sql
@@ -1,7 +1,10 @@
 -- models/input/user_session_channel.sql
-select
-  userId,
-  sessionId,
-  channel
-from {{ source('raw', 'user_session_channel') }}
-where sessionId is not null
+with user_session_channel as (
+  select
+    userId,
+    sessionId,
+    channel
+  from {{ source('raw', 'user_session_channel') }}
+  where sessionId is not null
+
+)
