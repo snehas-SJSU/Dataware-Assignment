c9844520611b6aaee0b286ebd532797df314d07e
snehas-SJSU <sneha.singh@sjsu.edu>
2025-11-04 12:10:39 -0800

    Update session_timestamp.sql

--- build_mau/models/input/session_timestamp.sql
+++ build_mau/models/input/session_timestamp.sql
@@ -1,7 +1,11 @@
 -- models/input/session_timestamp.sql
 -- Input model built as a CTE (ephemeral via dbt_project.yml)
-select
-  sessionId,
-  ts
-from {{ source('raw', 'session_timestamp') }}
-where sessionId is not null
+with session_timestamp as (
+  select
+    sessionId,
+    ts
+  from {{ source('raw', 'session_timestamp') }}
+  where sessionId is not null
+  )
+
+
